0e8c9e132be0877ef512171431c9adf4023df3bc
Paul Best <paul.best@lis-lab.fr>
2022-11-07 17:29:11 +0100

    fix plot result

--- plot_results_hdbcsan.py
+++ plot_results_hdbcsan.py
@@ -5,25 +5,28 @@ import numpy as np
 from sklearn import metrics
 
 species = np.loadtxt('good_species.txt', dtype=str)
-frontends = ['16_pcenMel128', '16_logMel128', '16_logSTFT', '16_Mel128', '8_pcen64', '32_pcenMel128']
+frontends = ['16_pcenMel128', '16_logMel128', '16_logSTFT', '16_Mel128', '8_pcenMel64', '32_pcenMel128']
 plt.figure()
 for specie in species:
     df = pd.read_csv(f'{specie}/{specie}.csv')
+    nmis = []
     for i, frontend in enumerate(frontends):
         print(specie, frontend)
         dic = np.load(f'{specie}/encodings_{specie}_{frontend}_sparrow_encoder_decod2_BN_nomaxPool.npy', allow_pickle=True).item()
         idxs, encodings, X = dic['idxs'], dic['encodings'], dic['umap']
-        
-        clusters = hdbscan.HDBSCAN(min_cluster_size=5, min_samples=None, cluster_selection_epsilon=0.0, core_dist_n_jobs=-1, cluster_selection_method='best').fit_predict(X)
+
+        clusters = hdbscan.HDBSCAN(min_cluster_size=100, min_samples=20, cluster_selection_epsilon=0.05, core_dist_n_jobs=-1, cluster_selection_method='leaf').fit_predict(X)
         df.loc[idxs, 'cluster'] = clusters.astype(int)
         mask = ~df.loc[idxs].label.isna()
         clusters, labels = clusters[mask], df.loc[idxs[mask]].label
-        plt.scatter(metrics.normalized_mutual_info_score(labels, clusters), i, label=specie)
-        df.drop('cluster', inplace=True)
+        nmis.append(metrics.normalized_mutual_info_score(labels, clusters))
+        df.drop('cluster', axis=1, inplace=True)
+    plt.scatter(nmis, np.arange(len(frontends)), label=specie)
 
-plt.ytick_labels(range(len(frontends)), frontends)
+plt.yticks(range(len(frontends)), frontends)
 plt.ylabel('archi')
 plt.xlabel('NMI with expert labels')
 plt.grid()
+plt.tight_layout()
 plt.legend()
 plt.savefig('NMIs_hdbscan.pdf')
--- plot_results_kmeans.py
+++ plot_results_kmeans.py
@@ -6,15 +6,16 @@ from sklearn import metrics, cluster
 from scipy.stats import linregress
 
 species = np.loadtxt('good_species.txt', dtype=str)
-frontends = ['16_pcenMel128', '16_logMel128', '16_logSTFT', '16_Mel128', '8_pcen64', '32_pcenMel128']
+frontends = ['16_pcenMel128', '16_logMel128', '16_logSTFT', '16_Mel128', '8_pcenMel64', '32_pcenMel128']
 plt.figure()
 for specie in species:
     df = pd.read_csv(f'{specie}/{specie}.csv')
+    nmis = []
     for i, frontend in enumerate(frontends):
         print(specie, frontend)
         dic = np.load(f'{specie}/encodings_{specie}_{frontend}_sparrow_encoder_decod2_BN_nomaxPool.npy', allow_pickle=True).item()
         idxs, encodings, X = dic['idxs'], dic['encodings'], dic['umap']
-        
+
         ks = (5*1.2**np.arange(20)).astype(int)
         distorsions = [cluster.KMeans(n_clusters=k).fit(encodings).inertia_ for k in ks]
         errors = [linregress(ks[:i], distorsions[:i]).stderr + linregress(ks[i+1:], distorsions[i+1:]).stderr for i in range(2, len(ks)-2)]
@@ -24,12 +25,14 @@ for specie in species:
 
         mask = ~df.loc[idxs].label.isna()
         clusters, labels = clusters[mask], df.loc[idxs[mask]].label
-        plt.scatter(metrics.normalized_mutual_info_score(labels, clusters), i, label=specie)
-        df.drop('cluster', inplace=True)
+        nmis.append(metrics.normalized_mutual_info_score(labels, clusters))
+        df.drop('cluster', axis=1, inplace=True)
+    plt.scatter(nmis, np.arange(len(frontends)), label=specie)
 
-plt.ytick_labels(range(len(frontends)), frontends)
+plt.yticks(range(len(frontends)), frontends)
 plt.ylabel('archi')
 plt.xlabel('NMI with expert labels')
 plt.grid()
 plt.legend()
+plt.tight_layout()
 plt.savefig('NMIs_kmeans.pdf')
